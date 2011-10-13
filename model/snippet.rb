b3b847b0380f2b33995fc3f33234c03be3daf370
teenst <teens.twenties@gmail.com>
2011-10-14 01:05:56 +0900

    split header & body

--- model/snippet.rb
+++ model/snippet.rb
@@ -7,11 +7,20 @@ module Model
     
     def self.create(data)
       #バリデーションをする
+      data.merge! self.parse(data[:code])
       id = self.collection.insert(data)
 
       self.collection.find_one(_id: id)
-      
     end
-    
+
+    def self.parse(code)
+      separator = /^#[ \t]*--\s*\r\n/
+      return {body: code} unless  code =~ separator
+      header, body = code.split(separator)
+      
+      p header
+      p body
+      {body: body}
+    end
   end
 end
