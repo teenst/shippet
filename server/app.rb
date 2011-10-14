b0991d8eb96ad530f7d42efd02627f3a23be78f0
phelrine <phelrine@gmail.com>
2011-10-14 18:37:07 +0900

    ユーザー認証

--- server/app.rb
+++ server/app.rb
@@ -1,8 +1,21 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'model'
+require 'omniauth'
+require 'openssl'
+require 'pp'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 class App < Sinatra::Base
+
+  use Rack::Session::Cookie, :expire_after => 3600 * 24, :secret => "change me"
+
+  use OmniAuth::Builder do
+    pp ENV
+    provider :twitter, ENV["TW_TOKEN"], ENV["TW_SECRET"]
+    provider :github, ENV["GH_TOKEN"], ENV["GH_SECRET"]
+  end
 
   helpers do
     alias_method :h, :escape_html
@@ -24,4 +37,8 @@ class App < Sinatra::Base
 
   end
 
+  get '/auth/:provider/callback' do |provider|
+    pp request.env["omniauth.auth"]
+    "#{provider} ok"
+  end
 end
