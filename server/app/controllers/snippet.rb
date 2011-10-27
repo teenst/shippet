e613baf1b6035461401732b89a4df3a177319329
teenst <teens.twenties@gmail.com>
2011-10-27 16:11:18 +0900

    Controller の 作成

---
+++ server/app/controllers/snippet.rb
@@ -0,0 +1,29 @@
+Shippet.controllers :snippet do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :show do
+    "show snippet"
+  end
+
+  post :create do
+    "post snippet"
+  end
+
+end
